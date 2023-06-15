0ab5a83ba1874d7bb49bb3b1b8e132736f3d9fd0
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-15 17:15:13 -0500

    refactored spinner1

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const animationEffects = ['|', '/', '-', '\\', '|', '/', '-', '\\', '|'];
+let delay = 100;
+
+for ( let i = 0; i < animationEffects.length; i++) {
+setTimeout (() => {
+process.stdout.write(`\r${animationEffects[i]}     `);
+if(i === animationEffects.length - 1) {
+  console.log();
+}
+}, delay);
+
+delay += 200;
+}
