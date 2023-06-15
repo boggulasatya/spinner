7714e728c08ccb0fb02592809140ab5d2d49f7f3
Satyavathi Boggula <satyavathi.411@gmail.com>
2023-06-15 17:19:46 -0500

    formatted doc

--- spinner1.js
+++ spinner1.js
@@ -9,44 +9,44 @@ Spaces are added after the character so that the cursor moves far enough away fr
 Removing the space padding at the end of the strings will mean that the cursor will sit on top of the animation, making it harder to see. */
 
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
-setTimeout(( ) => {
+setTimeout(() => {
   process.stdout.write('\r|   ');
-  }, 100);
-  
-  
-  setTimeout(( ) => {
+}, 100);
+
+
+setTimeout(() => {
   process.stdout.write('\r/   ');
-  }, 300);
-  
-  
-  setTimeout(( ) => {
+}, 300);
+
+
+setTimeout(() => {
   process.stdout.write('\r-   ');
-  }, 500);
-  
-  setTimeout(( ) => {
+}, 500);
+
+setTimeout(() => {
   process.stdout.write('\r\\   ');
-  }, 700);
+}, 700);
 
-  setTimeout(() => {
+setTimeout(() => {
   process.stdout.write('\r|    ');
-  }, 900);
+}, 900);
 
-  
-  setTimeout(( ) => {
-    process.stdout.write('\r/   ');
-    }, 1100);
-    
-    
-    setTimeout(( ) => {
-    process.stdout.write('\r-   ');
-    }, 1300);
-    
-    setTimeout(( ) => {
-    process.stdout.write('\r\\   ');
-    }, 1500);
-  
-    setTimeout(() => {
-    process.stdout.write('\r|    ');
-    }, 1700);
-  
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 1100);
+
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 1300);
+
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 1500);
+
+setTimeout(() => {
+  process.stdout.write('\r|    ');
+}, 1700);
+
 
